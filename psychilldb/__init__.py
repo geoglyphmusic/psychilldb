721b6cae14b1463f941b4f1c1348cac352bcd0ed
Chris <chrischarles89@yahoo.co.uk>
2020-08-25 10:34:12 +0100

    Environment variables added

--- psychilldb/__init__.py
+++ psychilldb/__init__.py
@@ -3,10 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///psychilldb.db'
-app.config['SECRET_KEY'] = 'the_secret_key'
+app.config['SECRET_KEY'] = os.environ.get('PSYCHILLDB_KEY')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -15,8 +16,8 @@ login_manager.login_message_category = 'info'# This sets the bootstrap class for
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'psychilldb@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Carthag13'
+app.config['MAIL_USERNAME'] = os.environ.get('PSYCHILLDB_EMAIL')
+app.config['MAIL_PASSWORD'] = os.environ.get('PSYCHILLDB_PASS')
 mail = Mail(app)
 
 from psychilldb import routes
